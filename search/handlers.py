46ded74b3d69e7f05d428c4b951763452249aeba
pencil <422870441@qq.com>
2019-03-03 16:15:05 +0800

    google search

--- search/handlers.py
+++ search/handlers.py
@@ -3,13 +3,34 @@
 
 from tornado.web import RequestHandler
 
-from search.func import WebSearch
+from search.func import WebSearch, GoogleSearch
 
 
 class WebSearchHandler(RequestHandler):
     def get(self):
-        websearch = WebSearch()
+        web_search = WebSearch()
         keywords = self.get_argument('keywords')
         search_target = int(self.get_argument('search_target', 1))
-        url = websearch.get_redirect_url(keywords, search_target)
+        url = web_search.get_redirect_url(keywords, search_target)
         self.redirect(url)
+
+
+class GoogleSearchHandler(RequestHandler):
+    def get(self):
+        keywords = self.get_argument('keywords')
+        page_num = self.get_argument('page_num')
+        google_search = GoogleSearch()
+        res = google_search.get_search_result(keywords, page_num)
+        '''
+        res = {
+            'success': True,
+            'msg': 'Sorry,出现故障了',
+            'data': [
+                {"search_type": "g", "keyword": "python", "page_num": 0, "title": "Python 1", "source": "http://www.runoob.com/python/python-intro.html", "des": "<div><span>这是详情<em>1<em></span></div>"},
+                {"search_type": "g", "keyword": "python", "page_num": 0, "title": "Python 2", "source": "http://www.runoob.com/python/python-intro.html", "des": "<div><span>这是详情<em>2<em></span></div>"},
+            ]
+        }
+        '''
+        self.set_header('Content-Type', 'application/json;charset=utf-8')
+        self.write(res)
+        # self.render('templates/search_results.html', res=res)
--- search/strategy.py
+++ search/strategy.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python3
 # -*- coding:utf-8 -*-
+import json
+
+from throttle.throttle.client import SearchClient
+from cache import cache_control
+from log import logger
 
 
 class KeywordsOperate:
@@ -24,3 +29,28 @@ class WordsSplit:
     @staticmethod
     def do_nothing(keywords):
         return [keywords, ]
+
+
+class GoogleSearchClient:
+    def __init__(self):
+        self.client = SearchClient()
+
+    # @cache_control(100)
+    def search(self, keyword, pn):
+        res = {
+            'success': False,
+            'msg': '',
+            'data': None,
+        }
+        try:
+            data = self.client.send(keyword, pn)
+        except Exception as e:
+            logger.error(e, exc_info=True)
+            data = 0
+
+        if data == 0:
+            res['success'] = False
+            res['msg'] = 'Sorry，出现故障了'
+        else:
+            res = data
+        return res
