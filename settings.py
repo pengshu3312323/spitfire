1abd79f10fca25bd3b94764a19c230799b0352e6
pencil <422870441@qq.com>
2019-03-03 22:03:14 +0800

    deploy

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 
-DEBUG = True
+DEBUG = False
 
 # 日志配置
 LOG_FILE_NAME = BASE_DIR + '/log/error.log'
@@ -14,13 +14,13 @@ LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 # redis
 if DEBUG:
     REDIS = {
-      'host': '127.0.0.1',
+      'host': 'localhost',
       'port': 6879,
-      'password': 'hummel165',
+      'password': 'password',
     }
 else:
     REDIS = {
-        'host': '172.17.0.1',
+        'host': 'host',
         'port': 6879,
-        'password': 'hummel165',
+        'password': 'password',
     }
